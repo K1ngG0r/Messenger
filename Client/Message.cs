f8d5ff5d64dacb326222b418b83da2bc4156285b
K1ngG0r <kinggor592@gmail.com>
2025-12-24 22:42:19 +0700

    Client-base

---
+++ Client/Message.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared;
+
+public sealed record Request(
+    RequestMethod Method,
+    string Body);
+
+public sealed record Response(
+    ResponseStatusCode Code,
+    string Body);
+
+public enum RequestMethod
+{
+    Time = 0
+}
+public enum ResponseStatusCode
+{
+    Ok = 0,
+    Failed = 1
+}
---
+++ Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using Shared;
+
+var udpClient = new UdpClient();
+var serverEndPoint = new IPEndPoint(IPAddress.Loopback, 9000);
+udpClient.Connect(serverEndPoint);
+Console.WriteLine($"Клиент запущен: {udpClient.Client.LocalEndPoint}");
+
+var request = new Request(RequestMethod.Time, string.Empty);
+var requestString = JsonSerializer.Serialize(request);
+var requestBytes = Encoding.UTF8.GetBytes(requestString);
+await udpClient.SendAsync(requestBytes);
+Console.WriteLine("Запрос отправлен");
+
+var result = await udpClient.ReceiveAsync();
+var responseString = Encoding.UTF8.GetString(result.Buffer);
+var response = JsonSerializer.Deserialize<Response>(responseString);
+
+if (response.Code == ResponseStatusCode.Ok)
+{
+    Console.WriteLine(response.Body);
+}
+else
+{
+    Console.WriteLine($"Ошибка на сервере: {response.Body}");
+}
+Console.ReadLine();
