c3c10183fb61e3bd615f0f042efc710a3bbea72c
Dmitry785 <danchenko.d2020@mail.ru>
2025-12-25 19:50:27 +0300

    messages presentation fix

--- Client/ViewModels/ChatMessageViewModel.cs
+++ Client/ViewModels/ChatMessageViewModel.cs
@@ -5,25 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Client.ViewModels
 {
     public class ChatMessageViewModel:ViewModel
     {
+        private string _currentUser;
         public string Message { get; set; }
         public string Name { get; set; }
         public string Username { get; set; }
         public DateTime When { get; set; }
         public string State { get; set; }
-        public HorizontalAlignment IsMe
+        public Dock IsMe
         {
             get 
             {
-                return HorizontalAlignment.Left;
+                return (Username == _currentUser) ? Dock.Right : Dock.Left;
             }
         }
-        public ChatMessageViewModel(ChatMessage message)
+        public ChatMessageViewModel(ChatMessage message, string currentUser="me")
         {
+            _currentUser = currentUser;
             Message = message.Message;
             Username = message.Who.Username;
             When = message.When;
