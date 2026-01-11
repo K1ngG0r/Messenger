dedb2a6739fcb9d125a68d638e04eefa31ea52b4
Dmitry785 <danchenko.d2020@mail.ru>
2026-01-11 08:09:54 +0300

    add update response

---
+++ Client/Connection/UpdateResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Connection
+{
+    public sealed record UpdateResponse(
+        UpdateResponseMethod method,
+        string payload);
+    public enum UpdateResponseMethod
+    {
+        NewMessage,
+        NewChat
+    }
+}
