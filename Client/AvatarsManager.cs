2fdccc3b6f75a788e13b87873828fe0deec4005c
Dmitry785 <danchenko.d2020@mail.ru>
2026-01-13 11:09:46 +0300

    avatars fix

--- Client/AvatarsManager.cs
+++ Client/AvatarsManager.cs
@@ -9,12 +9,29 @@ namespace Client
 {
     public static class AvatarsManager
     {
-        public static string GetChatAvatarPathByUsername(string username)
+        public static string GetUserAvatarPathByUsername(string username)
         {
             string previousPath = "C:/Users/U$er/Downloads/Messenger/Client";
-            if (File.Exists(previousPath + $"/cache/avatars/{username}.png")) 
-                return previousPath + $"/cache/avatars/{username}.png";
+            if (File.Exists(previousPath + $"/cache/avatars/users/{username}.png")) 
+                return previousPath + $"/cache/avatars/users/{username}.png";
             return previousPath + $"/cache/avatars/_default.png";
+        }
+        public static string GetChatAvatarByChatId(int chatId)
+        {
+            string previousPath = "C:/Users/U$er/Downloads/Messenger/Client";
+            if (File.Exists(previousPath + $"/cache/avatars/chats/id_{chatId}.png"))
+                return previousPath + $"/cache/avatars/chats/id_{chatId}.png";
+            return previousPath + $"/cache/avatars/_default.png";
+        }
+        public static void SetChatAvatarByChatId(int chatId, byte[] avatarBytes)
+        {
+            //можно обновить или сохранить аватарку для чата
+            //fixit
+        }
+        public static void SetUserAvatarPathByUsername(int username, byte[] avatarBytes)
+        {
+            //можно обновить или сохранить аватарку для пользователя
+            //fixit
         }
     }
 }
